ffc146b6a0d4e4dd290ff3c87ebd748b279071b2
Georgyi <112905032+georgij-tsabiev@users.noreply.github.com>
2025-04-15 15:46:15 +0300

    Подключение MySQL

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module demo/app-1
 
 go 1.24.2
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,44 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html/template"
-	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct { // Структура для хранения данных пользователя
-	Name                   string
-	Age                    uint16
-	Balance                int16
-	Avg_deposit, Happiness float64
-	Hobbies                []string
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
-func (u User) getAllInfo() string { // Метод возвращает всю информацию о пользователе одной строкой
-	return fmt.Sprintf("User name is: %s. He is %d and he has a balance of %d. \nAverage deposit: %.2f. Happiness: %.2f", u.Name, u.Age, u.Balance, u.Avg_deposit, u.Happiness)
-}
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang") // Открываем соединение с базой данных MySQL
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close() // Отложенное закрытие соединения с базой данных
 
-func (u *User) setNewName(newName string) { // Метод изменяет имя пользователя
-	u.Name = newName
-}
+	// Установка данных
+	// insert, err := db.Query("INSERT INTO users (name,age) VALUES ('Alex',25)")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer insert.Close() // Отложенное закрытие запроса
 
-func home_page(w http.ResponseWriter, r *http.Request) { // Обработчик главной страницы
-	Go := User{"Go", 24, 1000, 4.2, 0.8, []string{"Code", "Sport", "Travel"}}
-	// fmt.Fprintf(w, `<b>Main Text</b>`)
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, Go)
-}
+	res, err := db.Query("SELECT `name`, `age` FROM `users`") // Выборка данных из таблицы users
+	if err != nil {
+		panic(err)
+	}
 
-func contacts_page(w http.ResponseWriter, r *http.Request) { // Обработчик страницы контактов
-	fmt.Fprintf(w, "Contacts page text")
-}
-
-func handleRequest() { // Настройка маршрутизации и запуск сервера
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts/", contacts_page)
-	http.ListenAndServe(":8080", nil)
-}
-
-func main() { // Точка входа в программу
-	handleRequest()
+	for res.Next() { // Проходим по результатам выборки
+		var user User
+		err = res.Scan(&user.Name, &user.Age) // Сканируем данные из результата запроса в переменную user
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(fmt.Sprintf("User: %s with age %d\n", user.Name, user.Age)) // Выводим информацию о пользователе
+	}
 }
