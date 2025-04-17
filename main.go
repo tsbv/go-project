0d8fe3896e1ddb6af9fa4b00ffacb0232a1cf472
Georgyi <112905032+georgij-tsabiev@users.noreply.github.com>
2025-04-17 12:26:07 +0300

    Обработка главной страницы

--- main.go
+++ main.go
@@ -1,42 +1,25 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	"html/template"
+	"net/http"
 )
 
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang") // Открываем соединение с базой данных MySQL
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close() // Отложенное закрытие соединения с базой данных
-
-	// Установка данных
-	// insert, err := db.Query("INSERT INTO users (name,age) VALUES ('Alex',25)")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer insert.Close() // Отложенное закрытие запроса
-
-	res, err := db.Query("SELECT `name`, `age` FROM `users`") // Выборка данных из таблицы users
-	if err != nil {
-		panic(err)
-	}
-
-	for res.Next() { // Проходим по результатам выборки
-		var user User
-		err = res.Scan(&user.Name, &user.Age) // Сканируем данные из результата запроса в переменную user
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf(fmt.Sprintf("User: %s with age %d\n", user.Name, user.Age)) // Выводим информацию о пользователе
-	}
+	handleFunc()
 }
---
+++ version_0.01/main.go
@@ -0,0 +1,42 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	"fmt"
+
+// 	_ "github.com/go-sql-driver/mysql"
+// )
+
+// type User struct {
+// 	Name string `json:"name"`
+// 	Age  int    `json:"age"`
+// }
+
+// func main() {
+// 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang") // Открываем соединение с базой данных MySQL
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer db.Close() // Отложенное закрытие соединения с базой данных
+
+// 	// Установка данных
+// 	// insert, err := db.Query("INSERT INTO users (name,age) VALUES ('Alex',25)")
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
+// 	// defer insert.Close() // Отложенное закрытие запроса
+
+// 	res, err := db.Query("SELECT `name`, `age` FROM `users`") // Выборка данных из таблицы users
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	for res.Next() { // Проходим по результатам выборки
+// 		var user User
+// 		err = res.Scan(&user.Name, &user.Age) // Сканируем данные из результата запроса в переменную user
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		fmt.Printf(fmt.Sprintf("User: %s with age %d\n", user.Name, user.Age)) // Выводим информацию о пользователе
+// 	}
+// }
